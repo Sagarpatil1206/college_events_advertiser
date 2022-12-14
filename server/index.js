e244adb06aa58f0af68bb0865306f3fe11419852
Sagar Patil <sagarpatil120601@gmail.com>
2022-12-14 15:48:36 +0530

    added .env file

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"
+import dotenv from 'dotenv'
 
 import router from "./routes/postRoutes.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -13,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', router);
 
-const CONNECTION_URL = 'mongodb+srv://sagarpatil1206:Sagar%401206@cluster0.n0l2nh1.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://sagarpatil1206:Sagar@1206@cluster0.n0l2nh1.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
