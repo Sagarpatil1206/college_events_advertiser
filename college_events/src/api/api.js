96d4fa16ce20dd2ec9afb7e5f4245d140a2fa545
Sagar Patil <sagarpatil120601@gmail.com>
2022-12-19 00:05:48 +0530

    added url

--- college_events/src/api/api.js
+++ college_events/src/api/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-// const url = 'http://localhost:5000'
+const url = 'https://dull-gold-bunny-cape.cyclic.app/posts'
 
-export const fetchPosts = (page) => axios.get(`/posts?page=${page}`);
-export const fetchPost = (id) => axios.get(`/posts/${id}`)
-export const createPost = (newPost) => axios.post(`/posts`,newPost);
-export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
-export const addComment = (finalComment,id) => axios.post(`/posts/${id}/addComment`,{finalComment})
-export const deletePost = (id) => axios.delete(`/posts/${id}`);
-export const updatePost = (id,updatedPost) => axios.patch(`/posts/${id}`,updatedPost)
+export const fetchPosts = (page) => axios.get(`${url}?page=${page}`);
+export const fetchPost = (id) => axios.get(`${url}/${id}`)
+export const createPost = (newPost) => axios.post(url,newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const addComment = (finalComment,id) => axios.post(`${url}/${id}/addComment`,{finalComment})
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const updatePost = (id,updatedPost) => axios.patch(`${url}/${id}`,updatedPost)
 
-export const fetchPostsBySearch = (searchQuery) => axios.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
+export const fetchPostsBySearch = (searchQuery) => axios.get(`${url}/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
 //we have sent the searchqueery containing the search keyword and tags
