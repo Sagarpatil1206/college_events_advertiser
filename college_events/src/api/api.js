32a84767b574e3339381dd606948ba0b57a40754
Sagar Patil <sagarpatil120601@gmail.com>
2022-12-18 23:28:00 +0530

    change paths

--- college_events/src/api/api.js
+++ college_events/src/api/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000'
+// const url = 'http://localhost:5000'
 
-export const fetchPosts = (page) => axios.get(`${url}/posts?page=${page}`);
-export const fetchPost = (id) => axios.get(`${url}/posts/${id}`)
-export const createPost = (newPost) => axios.post(`${url}/posts`,newPost);
-export const likePost = (id) => axios.patch(`${url}/posts/${id}/likePost`);
-export const addComment = (finalComment,id) => axios.post(`${url}/posts/${id}/addComment`,{finalComment})
-export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
-export const updatePost = (id,updatedPost) => axios.patch(`${url}/posts/${id}`,updatedPost)
+export const fetchPosts = (page) => axios.get(`/posts?page=${page}`);
+export const fetchPost = (id) => axios.get(`/posts/${id}`)
+export const createPost = (newPost) => axios.post(`/posts`,newPost);
+export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
+export const addComment = (finalComment,id) => axios.post(`/posts/${id}/addComment`,{finalComment})
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
+export const updatePost = (id,updatedPost) => axios.patch(`/posts/${id}`,updatedPost)
 
-export const fetchPostsBySearch = (searchQuery) => axios.get(`${url}/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
+export const fetchPostsBySearch = (searchQuery) => axios.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
 //we have sent the searchqueery containing the search keyword and tags
