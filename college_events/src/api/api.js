e59e42d044814eb4e9cf3b19b4cb2b06a260ecfe
Sagar Patil <sagarpatil120601@gmail.com>
2022-12-19 20:23:52 +0530

    devtools disabled

--- college_events/src/api/api.js
+++ college_events/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const url = 'https://dull-gold-bunny-cape.cyclic.app/posts';
-const url = "http://localhost:5000/posts";
+const url = "https://college-events.onrender.com/posts";
 
 export const fetchPosts = (page) => axios.get(`${url}?page=${page}`);
 export const fetchPost = (id) => axios.get(`${url}/${id}`)
