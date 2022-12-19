8ce4e301d73dcdaa9fb06d40d907190f82639a65
Sagarpatil1206 <86339914+Sagarpatil1206@users.noreply.github.com>
2022-12-19 13:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('', router);
+
 app.use(express.static(path.join(__dirname,"./college_events/build")));
 app.get("*",function(req,res){
   res.sendFile(
@@ -28,7 +29,7 @@ app.get("*",function(req,res){
 
 // const CONNECTION_URL = 'mongodb+srv://sagarpatil1206:Sagar@1206@cluster0.n0l2nh1.mongodb.net/?retryWrites=true&w=majority';
 mongoose.set("strictQuery", false);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running successfully on Port ${PORT}`)))
